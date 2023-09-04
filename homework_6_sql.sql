bf1336137f70dd3bd162fc0a0be70a566660c6c9
AnastasiaKoliadina <118984315+AnastasiaKoliadina@users.noreply.github.com>
2023-09-04 21:06:47 +0400

    ДЗ_6
    Добавила дз_6

---
+++ homework_6_sql.sql
@@ -0,0 +1,61 @@
+DROP DATABASE IF EXISTS homework_6;
+CREATE DATABASE homework_6;
+USE homework_6;
+
+/*
+Создайте таблицу users_old, аналогичную таблице users. 
+Создайте процедуру, с помощью которой можно переместить любого (одного) пользователя из таблицы users в таблицу users_old. 
+(использование транзакции с выбором commit или rollback – обязательно).
+*/
+
+CREATE TABLE users_old (
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+    firstname VARCHAR(50), 
+    lastname VARCHAR(50), 
+    email VARCHAR(120)
+);
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS move_users;
+CREATE PROCEDURE  move_users (IN num1 INT) 
+	DETERMINISTIC
+BEGIN
+INSERT INTO users_old (firstname,lastname,email) 
+SELECT firstname, lastname, email 
+	FROM users 
+	WHERE users.id = num1;
+DELETE FROM users 
+	WHERE id = num1;
+COMMIT;
+END//
+
+DELIMITER ;
+
+CALL move_users();
+
+
+/*
+2. Создайте хранимую функцию hello(), которая будет возвращать приветствие, в зависимости от текущего времени суток. 
+С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать фразу "Добрый день", 
+с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+*/
+
+
+DELIMITER $$
+CREATE FUNCTION hello() 
+	RETURNS VARCHAR(25)
+	DETERMINISTIC
+BEGIN
+DECLARE result_text VARCHAR(25);
+SELECT CASE 
+	WHEN CURRENT_TIME >= '12:00:00' AND  CURRENT_TIME < '18:00:00' THEN 'Добрый день'
+	WHEN CURRENT_TIME >= '06:00:00' AND  CURRENT_TIME < '12:00:00' THEN 'Доброе утро'
+	WHEN CURRENT_TIME >= '00:00:00' AND  CURRENT_TIME < '06:00:00' THEN 'Доброй ночи'
+	ELSE 'Добрый вечер'
+END INTO result_text;
+RETURN result_text;
+END$$
+
+DELIMITER ;
+
+SELECT hello();
